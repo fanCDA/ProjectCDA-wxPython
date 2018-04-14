8643dca641968d8a84558ad5b73c0202c25666f1
fan CDA <fanCDApozdrawia@gmail.com>
2018-04-15 00:12:02 +0100

    Trying to add some more stuff: - Scrolled window with 8 column grid inside it - Fill grid with 100 Textfields

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
-# First things, first. Import the wxPython package.
 import wx
 
-# Next, create an application object.
-app = wx.App()
+class MyFrame ( wx.Frame ):
+	
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = "Hello World", pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+		
+		bSizer = wx.BoxSizer( wx.VERTICAL )
+		
+		self.m_scrolledWindow = wx.ScrolledWindow( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.VSCROLL )
+		self.m_scrolledWindow.SetScrollRate( 5, 5 )
+		gSizer = wx.GridSizer( 0, 8, 0, 0 )
 
-# Then a frame.
-frm = wx.Frame(None, title="Hello World")
+		for x in range(100):
+		
+                    textCtrl = wx.TextCtrl( self.m_scrolledWindow, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+                    gSizer.Add( textCtrl, 0, wx.ALL, 1 )
+		
+		
+		self.m_scrolledWindow.SetSizer( gSizer )
+		self.m_scrolledWindow.Layout()
+		gSizer.Fit( self.m_scrolledWindow )
+		bSizer.Add( self.m_scrolledWindow, 1, wx.EXPAND |wx.ALL, 5 )
+		
+		
+		self.SetSizer( bSizer )
+		self.Layout()
+		
+		self.Centre( wx.BOTH )
+	
+	def __del__( self ):
+		pass
 
-# Show it.
-frm.Show()
-
-# Start the event loop.
-app.MainLoop()
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MyFrame(None)
+    frame.Show()
+    app.MainLoop()
